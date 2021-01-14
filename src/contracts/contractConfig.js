52b39b193e8f58f690200d4a7f6e2037b4cb62ec
Anh Hoang Nguyen <anh.hoang.nguyen@advesa.com>
2021-01-14 13:32:20 +0700

    Update config to deploy smart contract

--- src/contracts/contractConfig.js
+++ src/contracts/contractConfig.js
@@ -4,17 +4,17 @@ module.exports = {
 
   RPC: process.env.NODE_ENV === 'production' ?
     'https://rpc.tomochain.com' :
-    'https://rpc.tomochain.com',
+    'https://rpc.testnet.tomochain.com',
 
   RPC_READ: process.env.NODE_ENV === 'production' ?
     'https://rpc.tomochain.com' :
-    'https://rpc.tomochain.com',
+    'https://rpc.testnet.tomochain.com',
 
   RPC_READ_SOCKET: process.env.NODE_ENV === 'production' ?
     'wss://ws.tomochain.com' :
     'wss://ws.tomochain.com',
 
-  NETWORK_ID: process.env.NODE_ENV === 'production' ? '88' : '88',
+  NETWORK_ID: process.env.NODE_ENV === 'production' ? '88' : '89',
   REFERRAL_ADDRESS: process.env.NODE_ENV === 'production' ?
     '0xce8419d22fa23196aeb354c028a231e51729f823' :
     '0xce8419d22fa23196aeb354c028a231e51729f823',
